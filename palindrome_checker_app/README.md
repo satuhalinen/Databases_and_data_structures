df83b5afee596c25a0d24519f2d0538ccabafb31
[satuhalinen] <satuh9@gmail.com>
2023-11-17 18:05:05 +0200

    new React project

---
+++ palindrome_checker_app/README.md
@@ -0,0 +1,3 @@
+# Palindrome checker app
+
+This is a palindrome checker app done on course called Databases and datastructures.
---
+++ palindrome_checker_app/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <p>This is palindrome checker.</p>
+    </>
+  );
+}
+
+export default App;
