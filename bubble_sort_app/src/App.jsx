83d941b40fe96e9675dab59bfc6cc7e6da146dcd
[satuhalinen] <satuh9@gmail.com>
2023-11-18 16:40:45 +0200

    add functionality for numbers 1-9

--- bubble_sort_app/src/App.jsx
+++ bubble_sort_app/src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 function App() {
+  const [arr, setSearch] = useState("");
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  };
+  const bubbleSort = () => {
+    let feed = arr.split(",");
+    for (let i = feed.length; i > 0; i--) {
+      for (let j = 0; j < i - 1; j++) {
+        if (feed[j] > feed[j + 1]) {
+          let temp = feed[j];
+          feed[j] = feed[j + 1];
+          feed[j + 1] = temp;
+        }
+      }
+    }
+    return feed.toString();
+  };
   return (
     <>
       <h1>Bubble sort app</h1>
+      <input type="text" onChange={searchHandler} />
+      <p>{bubbleSort()}</p>
     </>
   );
 }
