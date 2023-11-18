00c6160863cd162beea1214486b690eb69ebb1e6
[satuhalinen] <satuh9@gmail.com>
2023-11-18 17:07:14 +0200

    add functionality for all numbers

--- bubble_sort_app/src/App.jsx
+++ bubble_sort_app/src/App.jsx
@@ -7,16 +7,17 @@ function App() {
   };
   const bubbleSort = () => {
     let feed = arr.split(",");
-    for (let i = feed.length; i > 0; i--) {
+    let food = feed.map(Number);
+    for (let i = food.length; i > 0; i--) {
       for (let j = 0; j < i - 1; j++) {
-        if (feed[j] > feed[j + 1]) {
-          let temp = feed[j];
-          feed[j] = feed[j + 1];
-          feed[j + 1] = temp;
+        if (food[j] > food[j + 1]) {
+          let temp = food[j];
+          food[j] = food[j + 1];
+          food[j + 1] = temp;
         }
       }
     }
-    return feed.toString();
+    return food.toString();
   };
   return (
     <>
