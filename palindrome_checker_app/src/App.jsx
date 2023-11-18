df45cb0804d10511217d413fd6ac4763a865e938
[satuhalinen] <satuh9@gmail.com>
2023-11-18 15:28:17 +0200

    add functionality

--- palindrome_checker_app/src/App.jsx
+++ palindrome_checker_app/src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 function App() {
+  const [search, setSearch] = useState("");
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const palindromeChecker = () => {
+    let toArray = search.split("");
+    let toReverse = toArray.reverse();
+    let palindrome = toReverse.join("");
+    if (search == "") {
+      return "";
+    } else if (search == palindrome) {
+      return "is palindrome";
+    } else {
+      return "is not palidrome";
+    }
+  };
+
   return (
     <>
-      <p>This is palindrome checker.</p>
+      <p>This is a palindrome checker app.</p>
+      <input type="text" onChange={searchHandler} />
+      <p>{palindromeChecker()}</p>
     </>
   );
 }
