d15e25b0556175dce331cc8c9f7492771af8d287
[satuhalinen] <satuh9@gmail.com>
2023-11-25 15:07:15 +0200

    start a new project

---
+++ stack_data_structure_app/README.md
@@ -0,0 +1,3 @@
+# Stack data structure app
+
+This is a stack data structure app done on a course called Databases and data structures at Business College Helsinki.
---
+++ stack_data_structure_app/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Stack data structure app</h1>
+      <div></div>
+      <div></div>
+      <div></div>
+      <button></button>
+      <button></button>
+      <button></button>
+    </>
+  );
+}
+
+export default App;
