e62918c2826125321a0c731cdafe13a4aea0ce66
[satuhalinen] <satuh9@gmail.com>
2023-12-03 22:49:10 +0200

    add functionality to push the next number

--- stack_data_structure_app/src/App.jsx
+++ stack_data_structure_app/src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [stack, setStack] = useState([1, 2, 3]);
+  const addBoxHandler = () => {
+    let stackCopy = [...stack];
+    stackCopy.push(stack.length + 1);
+    setStack(stackCopy);
+  };
+  const listItems = stack.map((number) => <div>{number}</div>);
+
   return (
     <>
       <h1>Stack data structure app</h1>
-      <div className="blueBox"></div>
-      <div className="yellowBox"></div>
-      <div className="redBox"></div>
-      <button className="blueButton"></button>
+      {listItems}
+      <button className="blueButton" onClick={addBoxHandler}></button>
       <button className="yellowButton"></button>
       <button className="redButton"></button>
       <button className="removeButton"></button>
