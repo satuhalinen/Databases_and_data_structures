0d86cb3eb29c64f61cb45c161a30f9bac11cbb47
[satuhalinen] <satuh9@gmail.com>
2023-12-06 19:27:18 +0200

    add adding and removing colored rows instead of numbers

--- stack_data_structure_app/src/App.jsx
+++ stack_data_structure_app/src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const [stack, setStack] = useState([1, 2, 3]);
-  const addBoxHandler = () => {
+  let color;
+  const [stack, setStack] = useState([]);
+
+  const addBlueBoxHandler = () => {
     let stackCopy = [...stack];
-    stackCopy.push(stack.length + 1);
+    color = "blue";
+    stackCopy.push(color);
+    setStack(stackCopy);
+  };
+  const addYellowBoxHandler = () => {
+    let stackCopy = [...stack];
+    color = "yellow";
+    stackCopy.push(color);
+    setStack(stackCopy);
+  };
+  const addRedBoxHandler = () => {
+    let stackCopy = [...stack];
+    color = "red";
+    stackCopy.push(color);
     setStack(stackCopy);
   };
 
@@ -13,18 +28,23 @@ function App() {
     stackCopy.pop();
     setStack(stackCopy);
   };
-  const listItems = stack.map((number) => <div>{number}</div>);
+  const listItems = stack.map((color) => <div className={color}>{color}</div>);
 
   return (
     <>
       <h1>Stack data structure app</h1>
       {listItems}
-      <button className="blueButton" onClick={addBoxHandler}></button>
-      <button className="yellowButton"></button>
-      <button className="redButton"></button>
+      <button className="blueButton" onClick={addBlueBoxHandler}></button>
+      <button
+        className="yellowButton"
+        onClick={() => addYellowBoxHandler()}
+      ></button>
+      <button className="redButton" onClick={() => addRedBoxHandler()}></button>
       <button className="removeButton" onClick={removeBoxHandler}></button>
     </>
   );
 }
 
 export default App;
+
+//   onClick={() => clickHandler()}
