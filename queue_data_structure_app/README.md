cc7c026c7594e77f955d47896e53d52554b1db7f
[satuhalinen] <satuh9@gmail.com>
2023-12-07 18:34:11 +0200

    add the queue logic, add wagons, add README

--- queue_data_structure_app/README.md
+++ queue_data_structure_app/README.md
@@ -1,8 +1,11 @@
-# React + Vite
+# Queue data structure app
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a queue data structure app done a course called Databases and data structures at Business College Helsinki in December 2023. You can add green, orange and red wagons and remove them. The added wagon is put after the recently added wagon. When you remove, you remove the wagon that has been the longest time in the queue. The purpose of the task is to demonstrate the FIFO (First-In, First-Out) principle.
 
-Currently, two official plugins are available:
+## Technologies used
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- React
+
+## Visuals
+
+![Picture of the app](public/game.png)
--- queue_data_structure_app/src/App.jsx
+++ queue_data_structure_app/src/App.jsx
@@ -1,9 +1,59 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  let color;
+  const [stack, setStack] = useState([]);
+
+  const addGreenBoxHandler = () => {
+    let stackCopy = [...stack];
+    color = "greenWagon";
+    stackCopy.push(color);
+    setStack(stackCopy);
+  };
+  const addOrangeBoxHandler = () => {
+    let stackCopy = [...stack];
+    color = "orangeWagon";
+    stackCopy.push(color);
+    setStack(stackCopy);
+  };
+  const addRedBoxHandler = () => {
+    let stackCopy = [...stack];
+    color = "redWagon";
+    stackCopy.push(color);
+    setStack(stackCopy);
+  };
+
+  const removeBoxHandler = () => {
+    let stackCopy = [...stack];
+    stackCopy.shift();
+    setStack(stackCopy);
+  };
+  const listItems = stack
+    .toReversed()
+    .map((color) => <div className={color}></div>);
+
   return (
     <>
-      <h1>Queue data structure app</h1>
+      <div className="all">
+        <h1>Make a queue!</h1>
+        <div className="withBg">
+          <div className="wagons">{listItems}</div>
+          <div className="buttons">
+            <button className="greenButton" onClick={addGreenBoxHandler}>
+              Add a green wagon!
+            </button>
+            <button className="orangeButton" onClick={addOrangeBoxHandler}>
+              Add an orange wagon!
+            </button>
+            <button className="redButton" onClick={addRedBoxHandler}>
+              Add a red wagon!
+            </button>
+            <button className="removeButton" onClick={removeBoxHandler}>
+              Remove a wagon!
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
